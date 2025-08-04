eb61e5aac89dd2d718a58f044398f3c5f0821223
Sahilk2807 <kamblesahil2807@gmail.com>
2025-08-04 15:53:29 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,78 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
+
+// Layout and Core Components
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminProtectedRoute from './components/admin/AdminProtectedRoute';
+
+// Core Pages
 import HomePage from './pages/HomePage';
+import CoursesPage from './pages/CoursesPage';
+import CourseDetailPage from './pages/CourseDetailPage';
 import LoginPage from './pages/LoginPage';
-// ... import other pages
+import SignupPage from './pages/SignupPage';
+import DashboardPage from './pages/DashboardPage';
+
+// Static Pages
+import AboutUsPage from './pages/AboutUsPage';
+import ContactPage from './pages/ContactPage';
+// ... import other static pages
+
+// Admin Pages
+import AdminLoginPage from './pages/admin/AdminLoginPage';
+import AdminDashboard from './pages/admin/AdminDashboard';
+// ... import other admin pages like CourseManagement
 
 function App() {
+  // A layout for the main site (with Header/Footer)
+  const SiteLayout = () => (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow"><Outlet /></main>
+      <Footer />
+    </div>
+  );
+  
+  // A layout for the admin panel (could have a different header/sidebar)
+  const AdminLayout = () => (
+    // For now, it's just a simple outlet, but you could add an AdminSidebar here
+    <div><Outlet /></div>
+  );
+
   return (
     <ThemeProvider>
       <AuthProvider>
         <Router>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                {/* Add all other routes here */}
-              </Routes>
-            </main>
-            <Footer />
-          </div>
+          <Routes>
+            {/* Admin Routes */}
+            <Route path="/admin/login" element={<AdminLoginPage />} />
+            <Route element={<AdminProtectedRoute />}>
+              <Route path="/admin" element={<AdminLayout />}>
+                <Route path="dashboard" element={<AdminDashboard />} />
+                {/* <Route path="courses" element={<CourseManagementPage />} /> */}
+              </Route>
+            </Route>
+
+            {/* Main Site Routes */}
+            <Route path="/" element={<SiteLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="courses" element={<CoursesPage />} />
+              <Route path="courses/:courseId" element={<CourseDetailPage />} />
+              <Route path="login" element={<LoginPage />} />
+              <Route path="signup" element={<SignupPage />} />
+              <Route path="about" element={<AboutUsPage />} />
+              <Route path="contact" element={<ContactPage />} />
+              
+              {/* Protected User Routes */}
+              <Route element={<ProtectedRoute />}>
+                <Route path="dashboard" element={<DashboardPage />} />
+              </Route>
+            </Route>
+          </Routes>
         </Router>
       </AuthProvider>
     </ThemeProvider>

0bdbcfc18ebb2b5fbd02b4967aadcd4447c62fd9
Sahilk2807 <kamblesahil2807@gmail.com>
2025-08-04 16:23:46 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api', // Your Flask API URL
+  baseURL: 'https://learn-villa-api.onrender.com', // Your Flask API URL
 });
 
 // Interceptor to add the JWT token to requests
